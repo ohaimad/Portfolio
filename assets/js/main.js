8c09b3824dc2e733c86d27e391067e78677f9ee3
Otmane Haimad <ohaimad@e3r6p4.1337.ma>
2023-09-04 14:49:07 +0100

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -251,29 +251,61 @@
 
 })()
 
-const typewriterTexts = document.querySelectorAll('.typewriter');
-typewriterTexts.forEach((typewriterText) => {
-  const words = typewriterText.innerText.split(' ');
-  typewriterText.innerText = '';
+// const typewriterTexts = document.querySelectorAll('.typewriter');
+// typewriterTexts.forEach((typewriterText) => {
+//   const words = typewriterText.innerText.split(' ');
+//   typewriterText.innerText = '';
 
-  let i = 0;
-  let j = 0;
+//   let i = 0;
+//   let j = 0;
 
-  function typeWriter() {
-    if (i < words.length) {
-      if (j < words[i].length) {
-        typewriterText.innerHTML += words[i].charAt(j);
-        j++;
-        setTimeout(typeWriter, 100); // Adjust the typing speed (milliseconds)
-      } else {
-        i++;
-        j = 0;
-        typewriterText.innerHTML += ' ';
-        setTimeout(typeWriter, 400); // Adjust the delay between words (milliseconds)
-      }
+//   function typeWriter() {
+//     if (i < words.length) {
+//       if (j < words[i].length) {
+//         typewriterText.innerHTML += words[i].charAt(j);
+//         j++;
+//         setTimeout(typeWriter, 100); // Adjust the typing speed (milliseconds)
+//       } else {
+//         i++;
+//         j = 0;
+//         typewriterText.innerHTML += ' ';
+//         setTimeout(typeWriter, 400); // Adjust the delay between words (milliseconds)
+//       }
+//     }
+//   }
+
+//   // Start the typewriter effect
+//   setTimeout(typeWriter, 1500); // Adjust the delay before starting the animation (milliseconds)
+// });
+const dynamicText = document.querySelector(".txt span");
+const words = ["Graphic", "UX/UI"];
+
+// Variables to track the position and deletion status of the word
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChar;
+    dynamicText.classList.add("stop-blinking");
+
+    if (!isDeleting && charIndex < currentWord.length) {
+        // If condition is true, type the next character
+        charIndex++;
+        setTimeout(typeEffect, 200);
+    } else if (isDeleting && charIndex > 0) {
+        // If condition is true, remove the previous character
+        charIndex--;
+        setTimeout(typeEffect, 100);
+    } else {
+        // If word is deleted then switch to the next word
+        isDeleting = !isDeleting;
+        dynamicText.classList.remove("stop-blinking");
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200);
     }
-  }
+}
 
-  // Start the typewriter effect
-  setTimeout(typeWriter, 1500); // Adjust the delay before starting the animation (milliseconds)
-});
+typeEffect();
